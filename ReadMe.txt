05c15b9b0c83e04b69010b264bed5cf2e8264968
User <User@DESKTOP-QHG18FL>
2025-10-09 18:39:27 +0300

    Tank DONE

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,7 +1,12 @@
 ﻿https://github.com/okovtun/P_421.git
 https://www.youtube.com/playlist?list=PLeqyOOqxeiIOkhSw2O_jcH4ud2dGc1wMs
 
+https://www.codewars.com/kata/578b4f9b7c77f535fc00002f
+
 TODO:
+1. Нарисовать UML-диаграмму компьютера;
+
+DONE:
 1. Скачать и установить Visual Paradigm Community Edition:
 	https://www.visual-paradigm.com/download/community.jsp
 2. Зарегистрироваться на GitHub.com, если регистрировались ранее,
---
+++ UML/Car/main.cpp
@@ -0,0 +1,66 @@
+﻿#pragma warning(disable:4326)
+#include<iostream>
+using namespace std;
+
+#define MIN_TANK_VOLUME 30
+#define MAX_TANK_VOLUME 120
+class Tank
+{
+	const int VOLUME;	//Объем бака
+	double fuel_level;	//Уровень топлива
+public:
+	int get_VOLUME()const
+	{
+		return VOLUME;
+	}
+	double get_fuel_level()const
+	{
+		return fuel_level;
+	}
+	void fill(double amount)
+	{
+		if (amount < 0) return;
+		if (fuel_level + amount < VOLUME)fuel_level += amount;
+		else fuel_level = VOLUME;
+	}
+	Tank(int volume):
+		VOLUME
+	(
+		volume < MIN_TANK_VOLUME ? MIN_TANK_VOLUME :
+		volume > MAX_TANK_VOLUME ? MAX_TANK_VOLUME :
+		volume
+	)
+	{
+	//	VOLUME = volume;
+	//	l-value = r-value;
+		//	l-value - это переменная слева от оператора 'присвоить' =
+		fuel_level = 0;
+		cout << "Tank is ready:\t" << this << endl;
+	}
+	//expression - выражение
+	~Tank()
+	{
+		cout << "Tank is over:\t" << this << endl;
+	}
+
+	void info()const
+	{
+		cout << "Volume:\t\t" << VOLUME << " liters.\n";
+		cout << "Fuel level:\t" << fuel_level << " liters.\n";
+	}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	cout << "Hello Car" << endl;
+	Tank tank(-60);
+	tank.info();
+	do
+	{
+		double fuel;
+		cout << "Введите объем топлива: "; cin >> fuel;
+		tank.fill(fuel);
+		tank.info();
+	} while (true);
+}
